5ad38a9e0bbaa634fc2ec23afd44cbbde1312c8d
Kokkiri421 <trhb421@gmail.com>
2022-10-26 16:25:37 +0300

    dynamic import of component

--- components/canvas.tsx
+++ components/canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { Container, Stage } from '@inlet/react-pixi';
 import { observer } from 'mobx-react-lite';
 import CanvasGrid from './pixi/grid';
@@ -6,36 +6,35 @@ import useCanvasCamera from '../hooks/useCanvasCamera';
 import cameraStore from '../stores/cameraStore';
 
 const CanvasComponent = observer(() => {
-  const [renderClient, setRenderClient] = useState<boolean>(false);
   const ref = useRef<HTMLDivElement>(null);
   useCanvasCamera(ref);
 
-  useEffect(() => {
-    setRenderClient(true);
-  }, []);
-
   return (
     <div ref={ref}>
-      {renderClient && (
-        <Stage width={300} height={300} raf={false} renderOnComponentChange>
-          <Container
-            {...{
-              width: 300,
-              height: 300,
-              x: 10,
-              y: 20,
-              scale: cameraStore.cameraProperties.zoom,
-            }}
-          >
-            <CanvasGrid
-              cols={3}
-              rows={3}
-              size={20}
-              scale={cameraStore.cameraProperties.zoom}
-            />
-          </Container>
-        </Stage>
-      )}
+      <Stage
+        width={300}
+        height={300}
+        raf={false}
+        renderOnComponentChange
+        options={{ autoStart: true }}
+      >
+        <Container
+          {...{
+            width: 300,
+            height: 300,
+            x: 10,
+            y: 20,
+            scale: cameraStore.cameraProperties.zoom,
+          }}
+        >
+          <CanvasGrid
+            cols={3}
+            rows={3}
+            size={20}
+            scale={cameraStore.cameraProperties.zoom}
+          />
+        </Container>
+      </Stage>
     </div>
   );
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
 import styles from '../styles/Home.module.css';
-import CanvasComponent from '../components/canvas';
 
-const Home: NextPage = () => (
-  <div className={styles.container}>
-    <CanvasComponent />
-  </div>
-);
+const Home: NextPage = () => {
+  const CanvasComponent = dynamic(() => import('../components/canvas'), {
+    ssr: false,
+  });
+
+  return (
+    <div className={styles.container}>
+      <CanvasComponent />
+    </div>
+  );
+};
 
 export default Home;
